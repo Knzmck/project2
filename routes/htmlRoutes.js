25e105dda5a05ac71bb5ebaf041c1e9c8263e765
Mackenzie Schutz <mshoots64@gmail.com>
2020-06-22 11:46:24 -0500

    add isAuthenticated reference to middleware

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,9 @@ const Router = require('express').Router;
 const db = require('../models');
 const htmlRoutes = new Router();
 
+// Requiring our custom middleware for checking if a user is logged in. Routes with 'isAuthenticated' passed through are restricted routes for members only. 
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 // HTML Route to index (render info on loading)
 htmlRoutes.get('/', async (req, res) => {
   if (req.user) {
@@ -17,20 +20,10 @@ htmlRoutes.get('/', async (req, res) => {
   res.render('signup')
 
 });
-// HTML Route to index (render info on loading)
-// htmlRoutes.get('/', async (req, res) => {
-//   const dbGroups = await db.Group.findAll({});
 
-//   res.render('index', {
-//     // You can render any other information here
-//     msg: 'Welcome to the HomePage!',
-//     Groups: dbGroups
-//   });
 
-// });
-
-// Render Group page with all content for 'study group'
-htmlRoutes.get('/group/:id', async (req, res) => {
+// Render Group page with all content for 'study group' 
+htmlRoutes.get('/group/:id',isAuthenticated, async (req, res) => {
   const contentOptions = {
     where: {
       id: req.params.id
