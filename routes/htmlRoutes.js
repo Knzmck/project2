ac043cafd31cd4bdffc7bf4200447aaf45015f6b
Mackenzie Schutz <mshoots64@gmail.com>
2020-06-22 14:12:27 -0500

    list of groups rendering on homepage

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,29 +5,43 @@ const htmlRoutes = new Router();
 // Requiring our custom middleware for checking if a user is logged in. Routes with 'isAuthenticated' passed through are restricted routes for members only. 
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-// HTML Route to index (render info on loading)
+// HTML Route to index (render info on loading) - must be logged in or will redirect you to sign up page
 htmlRoutes.get('/', async (req, res) => {
   if (req.user) {
-    const dbGroups = await db.Group.findAll({});
-    // Where groups have association with ID 
-
-    res.render('index', {
-      // You can render any other information here
-      msg: 'Welcome to the HomePage!',
-      Groups: dbGroups
-    });
+    res.redirect('/homepage');
   }
   res.render('signup')
 
 });
 
+// Route for homepage
+htmlRoutes.get('/homepage', async (req, res) => {
+    // Finding all groups to join
+    const dbGroups = await db.Group.findAll({});
+    // // Find all groups associated with this user
+    // const dbUserGroup = await db.GroupUser.findAll({
+    //   where: req.user.id
+    // })
+
+    res.render('index', {
+      // You can render any other information here
+      msg: 'Welcome to the HomePage!',
+      groups: dbGroups
+    });
+})
+
+
 // Route for login page
 htmlRoutes.get('/login', async (req, res) => {
  res.render('login')
 })
+// Route for signup page
+htmlRoutes.get('/signup', async (req,res) => {
+  res.render('signup')
+})
 
 
-// Render Group page with all content for 'study group' 
+// Render Group page with all content for group. For example, 'study group' 
 htmlRoutes.get('/group/:id',isAuthenticated, async (req, res) => {
   const contentOptions = {
     where: {
@@ -41,5 +55,23 @@ htmlRoutes.get('/group/:id',isAuthenticated, async (req, res) => {
     group: dbGroups
   });
 });
+
+
+
+// Render content page for specific resource(aka post) within group using group ID in route
+// htmlRoutes.get('/post/:id', async (req, res) => {
+//   const groupPosts = {
+//     where: {
+//       GroupId: req.params.id
+//     }
+//   };
+
+//   const dbPosts = await db.Post.find(groupPosts);
+//   // reference to specific handlebars page referring to group id above
+//   // res.render('content', {
+//   //   content: dbPosts
+//   // });
+//   res.send(dbPosts)
+// });
 
 module.exports = htmlRoutes;
